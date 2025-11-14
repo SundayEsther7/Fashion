2e350d1f75fb0e0669559f04dd20912a6ff71f84
SundayEsther7 <7sundayesther@gmail.com>
2025-11-14 20:51:25 +0300

    Update

--- backend/app.js
+++ backend/app.js
@@ -21,6 +21,9 @@ mongoose.connect(process.env.MONGO_URI)
   app.get("/", (req, res) => {
   res.send("Auth API is running!");
 }); 
+app.get("/api/health", (req, res) => {
+  res.json({ status: "ok" });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(` Server running on http://localhost:${PORT}`));
