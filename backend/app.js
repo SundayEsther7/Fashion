b6c4704c034ae265b57b4b0876dd38892911781f
SundayEsther7 <7sundayesther@gmail.com>
2025-11-14 23:13:53 +0300

    updated cors

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://urbanglide-ij8e.onrender.com",  // your frontend URL
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true, // if you plan to send cookies or auth headers
+}));
+
 app.use(express.json());
 
 // Health route **must be before any catch-all static middleware**
