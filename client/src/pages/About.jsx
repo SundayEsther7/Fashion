a3e242584e824fe2bd67607e9e0e55615a6085f9
SundayEsther7 <7sundayesther@gmail.com>
2025-11-15 16:07:19 +0300

    Update

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,7 @@ export default function About() {
   return (
     <div className="pt-24 px-6 bg-neutralLight">
       <h1 className="text-4xl font-bold">About</h1>
-      <p>Here we will later show the about section.</p>
+      <p>Here we will later show the about section soon.</p>
     </div>
   );
 }
